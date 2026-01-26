115eb8a74f21e1dd0acb8c1a32743cc1f7b6a42c
José António Gomes <jose.antonio.luanda@gmail.com>
2026-01-26 17:21:59 +0000

    chore: update firebase config and rules for RBAC migration

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBRprh1ZLX0cLzf90H_OMxqcCvF654ivM4",
